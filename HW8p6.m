200ab0c3c424cda82d42a93790bd78b26fda6c22
johnno <stanleyswierczek@gmail.com>
2018-04-26 13:32:39 -0700

    huh?

--- HW8p6.m
+++ HW8p6.m
@@ -6,14 +6,14 @@ clear
 %% preliminaries
 colors
 dt = 0.01;
-Q = 1;
+Q = 0.1*dt;
 R = 1;
 spinsteps = 100000;
 nsteps = 20000;
-n = 40;
+n = 10;
 Traj = zeros(n,nsteps);
 PF = zeros(n,nsteps);
-Ne = 100;
+Ne = 400;
 w = zeros(1,Ne);
 color1 = 11;
 color2 = 9;
@@ -27,34 +27,39 @@ yy = 2;
 zz = 3;
 [M,N,H] = makeL96matrices(n);
 F = 8;
+szh = size(H,1);
+szq = size(H,2);
+QM = Q.*eye(szq);
+sqrtQ = sqrtm(QM);
+RM = R.*eye(szh);
 %%
 
 %% initial spin up
 Wallaby = randn(n,1);
 
 for ii=2:nsteps
-    Wallaby = lorenz96s4(Wallaby,dt,M,N,F);%+sqrt(dt).*randn(n,1);
+    Wallaby = lorenz96s4(Wallaby,dt,M,N,F)+sqrtQ*randn(n,1);
 end
 %%
 
 %% true state
 Traj(:,1) = Wallaby;
 for ii=2:nsteps
-    Traj(:,ii) = lorenz96s4(Traj(:,ii-1),dt,M,N,F);%+sqrt(dt).*randn(n,1);
+    Traj(:,ii) = lorenz96s4(Traj(:,ii-1),dt,M,N,F)+sqrtQ*randn(n,1);
 end
 %%
 
 %% observations
-Obs = H*Traj +R.*randn(20,nsteps);
+Obs = H*Traj +R.*randn(n/2,nsteps);
 %%
 
 %% ensemble + simulation
 Ens = ensemble_init(dt,Ne,M,N,F,Wallaby);
 
 for jj=2:nsteps
-    Ens = lorenz96s4(Ens,dt,M,N,F) + sqrt(dt).*randn(n,Ne);
+    Ens = lorenz96s4(Ens,dt,M,N,F) + sqrtQ*randn(n,Ne);
     for kk=1:Ne
-        w(kk) = 0.5.*(Obs(:,jj)-H*Ens(:,kk))'*(eye(20)./R)*(Obs(:,jj)-H*Ens(:,kk));
+        w(kk) = 0.5.*(Obs(:,jj)-H*Ens(:,kk))'*(eye(n/2)./R)*(Obs(:,jj)-H*Ens(:,kk));
     end
     W = normalizeweights(w);
     Ens = resamplingmmo(W,Ens,Ne,n);
--- HW9p2.m
+++ HW9p2.m
@@ -6,3 +6,6 @@ set(0,'DefaultFigureVisible','off')
 
 %% preliminaries
 colors
+chain_length = 10e5;
+n = 10;
+beta = 1/sqrt(n);
